340fcae683e752f505dd76717530e4bd92a57f8e
An Nuyen <annuyen@gmail.com>
2019-04-25 14:46:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.scan(/^[aeiouAEIOU]/) ? true : false 
+  word.scan(/\b[aeiou][a-z]*\b/) ? true : false 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
